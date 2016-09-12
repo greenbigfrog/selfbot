9013aaab5d61a888ba86f9b89146bd6858f7d8ce
Jonathan B <greenbigfrog@gmail.com>
2016-09-12 14:20:45 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'pry'
+gem 'dotenv'
+gem 'discordrb', git: 'git://github.com/meew0/discordrb.git'
---
+++ main.rb
@@ -0,0 +1,29 @@
+
+require 'bundler/setup'
+require 'pry'
+
+require 'dotenv'
+Dotenv.load
+
+require 'discordrb'
+bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], type: :user, prefix: '', advanced_functionality: false, help_command: false, parse_self: true, help_available: false, permission_message: false, debug: true #, log_mode: :debug
+
+# Set your ID here
+bot.set_user_permission(163607982473609216, 1)
+
+bot.command(:eval, permission_level: 1) do |event, *code|
+  input = code.join(' ')
+
+  output = eval(input)
+
+  event.message.edit "**Input**: `#{input}`\n**Output**: ```\n#{output}\n```" if output
+  event.message.delete if output.nil?
+  nil
+end
+
+bot.command(:pry, permission_level: 1) do |event|
+  event.message.delete
+  binding.pry
+end
+
+bot.run
