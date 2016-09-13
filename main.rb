6ebaeabba0478f08cc8010487ff03dad95fd3e43
Jonathan B <greenbigfrog@gmail.com>
2016-09-13 14:49:02 +0200

    Fix no permission issue

--- main.rb
+++ main.rb
@@ -6,12 +6,12 @@ require 'dotenv'
 Dotenv.load
 
 require 'discordrb'
-bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], type: :user, prefix: '', advanced_functionality: false, help_command: false, parse_self: true, help_available: false, permission_message: false, debug: true #, log_mode: :debug
+bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], type: :user, prefix: '', advanced_functionality: false, help_command: false, parse_self: true, help_available: false, debug: true #, log_mode: :debug
 
 # Set your ID here
 bot.set_user_permission(163607982473609216, 1)
 
-bot.command(:eval, permission_level: 1) do |event, *code|
+bot.command(:eval, permission_message: false, permission_level: 1) do |event, *code|
   input = code.join(' ')
 
   output = eval(input)
@@ -21,7 +21,7 @@ bot.command(:eval, permission_level: 1) do |event, *code|
   nil
 end
 
-bot.command(:pry, permission_level: 1) do |event|
+bot.command(:pry, permission_message: false, permission_level: 1) do |event|
   event.message.delete
   binding.pry
 end
